b5368a46b06980ebf913434ba40a0d20b67fae9b
Heyang Liu <lhypds@gmail.com>
2025-06-20 03:32:34 +0900

    feat: add buttons

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,9 +1,30 @@
-import styles from './home.module.css';
+import homeStyles from './home.module.css';
+import { Input, Button } from '@linktivity/link-ui';
 
 const HomeView = () => {
   return (
-    <div className={styles.home}>
-      <h1 className={styles.title}>{'Home'}</h1>
+    <div className={homeStyles.home}>
+      <Input
+        className={homeStyles.input}
+        style={{ textAlign: 'center' }}
+        defaultValue={'5:00'}
+      />
+      <div>
+        <Button className={homeStyles.button} variant="outlined">
+          {'+0:30'}
+        </Button>
+        <Button className={homeStyles.button} variant="outlined">
+          {'+1:00'}
+        </Button>
+        <Button className={homeStyles.button} variant="outlined">
+          {'+5:00'}
+        </Button>
+      </div>
+      <div>
+        <Button className={homeStyles.button} variant="outlined">
+          {'▶'}
+        </Button>
+      </div>
     </div>
   );
 };
