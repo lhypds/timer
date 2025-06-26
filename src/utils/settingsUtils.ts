d387eb7cf5c2959713f32d791e758383002a19b5
Heyang Liu <lhypds@gmail.com>
2025-06-26 09:40:46 +0900

    fix: use session storage for multiple start

--- src/utils/settingsUtils.ts
+++ src/utils/settingsUtils.ts
@@ -5,17 +5,17 @@ const MODE_INIT = Mode.Timer;
 
 export const getSetting = (key: string) => {
   if (key === 'timer') {
-    const time = localStorage.getItem('timer');
+    const time = sessionStorage.getItem('timer');
     return time ? parseInt(time, 10) : SECONDS_INIT;
   }
 
   if (key === 'stopwatch') {
-    const time = localStorage.getItem('stopwatch');
+    const time = sessionStorage.getItem('stopwatch');
     return time ? parseInt(time, 10) : 0;
   }
 
   if (key === 'mode') {
-    const mode = localStorage.getItem('mode');
+    const mode = sessionStorage.getItem('mode');
     return mode ? (mode as Mode) : MODE_INIT;
   }
 };
