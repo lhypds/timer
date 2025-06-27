34114f90e846f2c54be4a7c7960f11f103ccbaa6
Heyang Liu <lhypds@gmail.com>
2025-06-28 04:12:11 +0900

    feat: count to time timer mode
    feat: count to timer
    
    fix: wave dash state
    
    fix: font size of milliseconds
    
    feat: increase max time
    
    feat: input buffer support hour
    
    feat: display seconds
    
    fix: count to timer display
    
    feat: input buffer for debug
    
    fix: input buffer clear
    
    fix: milliseconds display
    
    feat: switch sub text when isEditing change
    
    fix: refresh count to timer
    
    fix: refresh count down timer
    
    fix: move refresh countToTimer
    
    feat: show count to
    
    refactor: reduce time utils
    
    feat: use the latest count to time
    
    fix: remove the mode countTo
    
    fix: small improve

--- src/utils/settingsUtils.ts
+++ src/utils/settingsUtils.ts
@@ -1,36 +1,62 @@
 import { Mode } from '../components/ModeSwitch/ModeSwitch';
 
-const SECONDS_INIT = 300;
-const MODE_INIT = Mode.Timer;
+const DEFAULT_TIMER_SECONDS = 300;
+const DEFAULT_MODE = Mode.Timer;
+const DEFAULT_IS_COUNT_TO_TIMER = false;
 
 export const getSetting = (key: string) => {
   if (key === 'timer') {
     const time = sessionStorage.getItem('timer');
-    return time ? parseFloat(time) : SECONDS_INIT;
+    return time ? parseFloat(time) : DEFAULT_TIMER_SECONDS;
   }
 
   if (key === 'timerInitial') {
     const time = sessionStorage.getItem('timerInitial');
-    return time ? parseFloat(time) : SECONDS_INIT;
+    return time ? parseFloat(time) : DEFAULT_TIMER_SECONDS;
   }
 
   if (key === 'stopwatch') {
     const time = sessionStorage.getItem('stopwatch');
-    return time ? parseFloat(time) : 0;
+    return time ? parseFloat(time) : 0; // default to 0 seconds
   }
 
   if (key === 'mode') {
     const mode = sessionStorage.getItem('mode');
-    return mode ? (mode as Mode) : MODE_INIT;
+    return mode ? (mode as Mode) : DEFAULT_MODE;
+  }
+
+  if (key === 'isCountToTimer') {
+    const isCountToTimer = sessionStorage.getItem('isCountToTimer');
+    return isCountToTimer
+      ? isCountToTimer === 'true'
+      : DEFAULT_IS_COUNT_TO_TIMER;
+  }
+
+  if (key === 'countTo') {
+    const countTo = sessionStorage.getItem('countTo');
+    return countTo ? parseInt(countTo) : null;
   }
 };
 
-export const setSetting = (key: string, value: string | number) => {
-  if (key === 'timer' || key === 'timerInitial' || key === 'stopwatch') {
+export const setSetting = (key: string, value: string | number | boolean) => {
+  // String
+  if (key === 'inputBuffer') {
+    sessionStorage.setItem('inputBuffer', value as string);
+    return;
+  }
+
+  // Number
+  if (
+    key === 'timer' ||
+    key === 'timerInitial' ||
+    key === 'stopwatch' ||
+    key === 'countTo'
+  ) {
     sessionStorage.setItem(key, value.toString());
     return;
   }
 
+  // Mode
   if (key === 'mode') {
     if (Object.values(Mode).includes(value as Mode)) {
       sessionStorage.setItem(key, value as Mode);
@@ -40,20 +66,26 @@ export const setSetting = (key: string, value: string | number) => {
     return;
   }
 
+  // Boolean
+  if (key === 'isCountToTimer') {
+    sessionStorage.setItem('isCountToTimer', value ? 'true' : 'false');
+    return;
+  }
+
   console.warn(`Unknown setting key: ${key}`);
 };
 
 export const initializeSettings = () => {
   if (sessionStorage.getItem('timer') === null) {
-    sessionStorage.setItem('timer', SECONDS_INIT.toString());
+    sessionStorage.setItem('timer', DEFAULT_TIMER_SECONDS.toString());
   }
   if (sessionStorage.getItem('timerInitial') === null) {
-    sessionStorage.setItem('timerInitial', SECONDS_INIT.toString());
+    sessionStorage.setItem('timerInitial', DEFAULT_TIMER_SECONDS.toString());
   }
   if (sessionStorage.getItem('stopwatch') === null) {
     sessionStorage.setItem('stopwatch', '0');
   }
   if (sessionStorage.getItem('mode') === null) {
-    sessionStorage.setItem('mode', MODE_INIT);
+    sessionStorage.setItem('mode', DEFAULT_MODE);
   }
 };
